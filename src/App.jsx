f284940d27941e530dfce7e46b43e6a603ed224e
nijanthan2805 <nijanthan1628@gmail.com>
2025-11-13 07:13:09 +0530

    Added react router to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import { Routes, Route } from 'react-router'
 import { HomePage } from './pages/HomePage'
 import './App.css'
 
 function App() {
   return (
-    <HomePage />
+    <Routes>
+      <Route path="/" element={<HomePage />}></Route>
+      <Route path="checkout" element={<div>Test checkout page</div>}></Route>
+    </Routes>
   )
 }
 
